44f45b86390a274e05274a17c0831b5e54f64c64
Artem Nikulin <itsacats@gmail.com>
2022-02-12 01:06:02 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const helloEl = document.getElementById('js-hello');
+    const devEl = document.getElementById('js-developer');
+
+    animatedType(helloEl, 'Hello, my name is Artem.');
+
+    function animatedType(el, text) {
+        let count = 0;
+        let result = '';
+
+        function typeLine() {
+            let interval = setTimeout(
+                () => {
+                    result += text[count]
+                    el.innerText = result + '|';
+
+                    count++;
+                    if (count >= text.length) {
+                        count = 0;
+                        clearTimeout(interval);
+                        el.innerText = result;
+                        if (!devEl.innerText.length) {
+                            animatedType(devEl, 'I\'M A DEVELOPER');
+                        }
+                        return;
+                    }
+                    typeLine();
+                }, getRandomInt(getRandomInt(700)))
+        }
+
+        typeLine();
+
+        function getRandomInt(max) {
+            return Math.floor(Math.random() * Math.floor(max));
+        }
+    }
+})
+
+$(function () {
+    $('.tabs tab-link').click(function(e){
+        e.preventDefault();
+        $(this).tab('show');
+    });
+})
